8df3bc741baaa457bfa1dd496dc07bdfc02cde6c
J-hoplin1 <jhoplin7259@gmail.com>
2024-04-13 17:46:13 +0900

    Feat: Add getuser decorator

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -1,3 +1,4 @@
 export * from './pagination/pagination.decorator';
 export * from './public/public.decorator';
 export * from './swagger';
+export * from './role/roles.decorator';
---
+++ src/decorator/user/get-user.decorator.ts
@@ -0,0 +1,29 @@
+// Nest Packages
+import { UserPayload } from '@app/types';
+import { ExecutionContext, createParamDecorator } from '@nestjs/common';
+
+// Third-party Packages
+import { Request } from 'express';
+
+/**
+ * @GetUser decorator
+ *
+ * @GetUser('id') => Get only id field of user
+ * @GetUser() => Get user credential
+ *
+ * Use this decorator in request handler for getting authenticated user credential
+ */
+
+export const GetUser = createParamDecorator(
+  (
+    data: keyof UserPayload,
+    context: ExecutionContext,
+  ): UserPayload | UserPayload[keyof UserPayload] => {
+    // Get request context
+    const request = context.switchToHttp().getRequest<Request>();
+
+    // Get request user
+    const user: UserPayload = (request.user ?? {}) as UserPayload;
+    return data ? user[data] : user;
+  },
+);
