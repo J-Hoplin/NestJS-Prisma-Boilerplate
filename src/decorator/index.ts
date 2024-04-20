5576b96d6c0fabff43452fafcea4d3fb51f5ccf0
J-hoplin1 <jhoplin7259@gmail.com>
2024-04-20 14:05:52 +0900

    Fix: Add import

--- src/decorator/index.ts
+++ src/decorator/index.ts
@@ -2,3 +2,4 @@ export * from './pagination/pagination.decorator';
 export * from './public/public.decorator';
 export * from './swagger';
 export * from './role/roles.decorator';
+export * from './user/get-user.decorator';
